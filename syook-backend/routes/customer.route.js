2681395b4808da8972c9e22dab87d90b2cfa4720
Bharatsharma <108224876+bharat24862486@users.noreply.github.com>
2023-08-25 13:28:06 +0530

    fixing errors

--- syook-backend/routes/customer.route.js
+++ syook-backend/routes/customer.route.js
@@ -1,5 +1,5 @@
 const express=require(`express`)
-const { CustomerModel } = require("../models/customer.model")
+const  CustomerModel  = require("../models/customer.model")
 
 const CustomerRouter=express.Router()
 
